372d224ea8f9d49bdf99882e881b3368a58a2b43
anatom3000 <49984698+anat3000@users.noreply.github.com>
2020-10-23 16:09:35 +0200

    Changes.

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from match import *
+import csv
+import pprint
+
+import match
+import utils
 
 # pour lire la base de données: https://docs.python.org/fr/3/library/csv.html
 
+players = []
+with open('Characters.csv', newline='') as f:
+    reader = csv.DictReader(f, delimiter=';')
+    for row in reader:
+        players.append(row)
 
-print("Bienvenue dans Super Quiditch !")
+print(utils.print_scoreboard(players, fields=["Id", "Name", "House", "Species"], limit=10))
