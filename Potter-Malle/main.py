87ea5479fc026992f5389bf6e50a3e0a6df9df7e
anatom <49984698+anatom3000@users.noreply.github.com>
2021-02-05 10:50:25 +0100

    Added REAL IHM

--- Potter-Malle/main.py
+++ Potter-Malle/main.py
@@ -6,6 +6,20 @@ Liste de fournitures scolaires
 Auteur : David Landry
 '''
 
+def print_results(data):
+    poids = 0
+    for k, v in enumerate(data):
+        print(f"### Objet n°{k+1} ###")
+        print(f"Nom : {v['Nom']}")
+        print(f"Poids : {v['Poids']}")
+        print(f"Mana : {v['Mana']}")
+        print()
+        poids += v["Poids"]
+    
+    print(f"Poids total : {round(poids, 3)} kg.")
+    
+        
+
 fournitures_scolaires = [{'Nom' : 'Manuel scolaire', 'Poids' : 0.6, 'Mana' : 11},
                          {'Nom' : 'Baguette magique', 'Poids' : 0.085, 'Mana' : 120},
                          {'Nom' : 'Chaudron', 'Poids' : 2.7, 'Mana' : 2},
@@ -24,7 +38,7 @@ def tri(data, key=lambda x: x, reverse=False):
     for i in range(len(data) - 1):
         mini = i
         for j in range(i + 1, len(data)):
-            if key(data[j]) < key(tab[mini]):
+            if key(data[j]) < key(data[mini]):
                 mini = j
         data[i], data[mini] = data[mini], data[i]
     return data
@@ -47,9 +61,11 @@ DEFINIR partieABC(fournitures, max_poids):
     RETOURNER mis_dans_malle
 FIN_DEFINIR
 """
-def partieABC(fournitures, max_poids, priorite=lambda x: x):
+def partieABC(fournitures, max_poids, priorite=None):
 
-    fournitures = tri(fournitures, key=priorite, reverse=True)
+    if priorite is not None:
+        fournitures = tri(fournitures, key=priorite, reverse=True)
+    
     mis_dans_malle = []
     for f in fournitures:
         poids_total = 0
@@ -60,6 +76,37 @@ def partieABC(fournitures, max_poids, priorite=lambda x: x):
 
     return mis_dans_malle
 
-partieABC(fournitures_scolaires, poids_maximal)                                # partie A
-partieABC(fournitures_scolaires, poids_maximal, priorite=lambda x: x["Poids"]) # partie B
-partieABC(fournitures_scolaires, poids_maximal, priorite=lambda x: x["Mana"] ) # partie C
+
+print("Bienvenue dans notre app de rangement de malle.")
+use_custom_fournitures = input("Voulez-vous entrer des fournitures personnalisées (y/n) ? ") in ['y', 'o', 'yes', 'oui']
+if use_custom_fournitures:
+    fournitures_scolaires = []
+    nb_fournitures = int(input("Entrez le nombre d'objets que vous voulez proposer : "))
+    for i in range(nb_fournitures):
+        objet = {}
+        print(f"### Objet n°{i+1} ###")
+        objet["Nom"] = input("Nom : ")
+        objet["Poids"] = float(input("Poids : "))
+        objet["Mana"] = float(input("Mana : "))
+        fournitures_scolaires.append(objet)
+
+
+input_poids = input("Entrez le poids maximal (laisser blanc pour défaut) : ")
+poids_maximal = int(input_poids) if input_poids != '' else poids_maximal
+
+print("[a] Remplir n'importe comment")
+print("[b] Remplir pour le plus lourd possible")
+print("[c] Remplir pour le plus de mana possible")
+mode = input("Entrez le mode de remplissage : ")
+while mode.lower() not in ["a", "b", "c"]:
+    print("Veuillez entrer une valeur valide (a/b/c)")
+    mode = input("Entrez le mode de remplissage : ")
+
+
+if mode.lower() == 'a':
+    print_results(partieABC(fournitures_scolaires, poids_maximal))
+elif mode.lower() == 'b':
+    print_results(partieABC(fournitures_scolaires, poids_maximal, priorite=lambda x: x["Poids"]))
+else:
+    print_results(partieABC(fournitures_scolaires, poids_maximal, priorite=lambda x: x["Mana"]))
+
